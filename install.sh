3646753ede581cb9c34315ca835fc8f4340121ae
Almighty42 <xenoman4202@gmail.com>
2025-05-30 11:34:12 +0200

    Changed install script again

--- install.sh
+++ install.sh
@@ -20,9 +20,9 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{} 2>/dev/null
 config fetch origin
-config reset --hard origin/main
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{} 2>/dev/null
+config reset --hard origin/HEAD
 config config status.showUntrackedFiles no
 rm -rf $HOME/README.md
 rm -rf $HOME/install.sh
