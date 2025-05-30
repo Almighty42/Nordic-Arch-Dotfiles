41061aeecd0725c493941e940917cf831954acbe
Almighty42 <xenoman4202@gmail.com>
2025-05-30 11:41:24 +0200

    Changed install script again 2#

--- install.sh
+++ install.sh
@@ -1,28 +1,16 @@
-# git clone --bare https://github.com/Almighty42/Nordic-Arch-Dotfiles.git $HOME/.cfg
-# function config {
-#    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-# }
-# mkdir -p .config-backup
-# config checkout
-# if [ $? = 0 ]; then
-#   echo "Checked out config.";
-#   else
-#     echo "Backing up pre-existing dot files.";
-#     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-# fi;
-# config checkout
-# config config status.showUntrackedFiles no
-# rm -rf $HOME/README.md
-# rm -rf $HOME/install.sh
-
 git clone --bare https://github.com/Almighty42/Nordic-Arch-Dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config fetch origin
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{} 2>/dev/null
-config reset --hard origin/HEAD
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
 config config status.showUntrackedFiles no
 rm -rf $HOME/README.md
 rm -rf $HOME/install.sh
