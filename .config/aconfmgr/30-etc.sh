1f5fac417005cc0926e2fb43906a8ed11e230b0f
Almighty42 <xenoman4202@gmail.com>
2025-06-17 21:17:55 +0200

    Testing some stuff out and figuring out why aconfmgr breaks lightdm on installed system 13#

--- .config/aconfmgr/30-etc.sh
+++ .config/aconfmgr/30-etc.sh
@@ -7,17 +7,17 @@
 CopyFile /etc/hostname
 CopyFile /etc/lightdm/lightdm-webkit2-greeter.conf
 CopyFile /etc/lightdm/lightdm.conf
-# CopyFile /etc/locale.conf
-# CopyFile /etc/locale.gen
-# CopyFile /etc/mkinitcpio.conf
+CopyFile /etc/locale.conf
+CopyFile /etc/locale.gen
+CopyFile /etc/mkinitcpio.conf
 CopyFile /etc/pacman.conf
 CopyFile /etc/pacman.d/mirrorlist
-# CopyFile /etc/shells
-# CopyFile /etc/subgid
-# CopyFile /etc/subuid
-# CopyFile /etc/systemd/zram-generator.conf
-# CopyFile /etc/updatedb.conf
-# CopyFile /etc/vconsole.conf
+CopyFile /etc/shells
+CopyFile /etc/subgid
+CopyFile /etc/subuid
+CopyFile /etc/systemd/zram-generator.conf
+CopyFile /etc/updatedb.conf
+CopyFile /etc/vconsole.conf
 
 IgnorePath "/etc/sudoers"
 IgnorePath "/etc/sudoers.d"
