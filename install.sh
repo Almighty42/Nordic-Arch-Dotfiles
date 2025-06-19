72c6d26adeee5c9b108e8156b2b2f2474eb02103
Almighty42 <xenoman4202@gmail.com>
2025-06-19 13:22:09 +0200

    Added folder creation to install script

--- install.sh
+++ install.sh
@@ -15,5 +15,31 @@ config config status.showUntrackedFiles no
 
 cp -rf $HOME/fonts/* $HOME/.local/share/fonts
 
+mkdir -p $HOME/Desktop
+mkdir -p $HOME/Development
+mkdir -p $HOME/Development/Code/
+mkdir -p $HOME/Development/Study/
+mkdir -p $HOME/Development/Code/Books-Code/
+mkdir -p $HOME/Development/Code/Courses-Code/
+mkdir -p $HOME/Development/Code/Projects-Code/
+mkdir -p $HOME/Development/Study/Books/
+mkdir -p $HOME/Development/Study/Courses/
+mkdir -p $HOME/Development/Study/Resources/
+mkdir -p $HOME/Documents/
+mkdir -p $HOME/Documents/EPUBs/
+mkdir -p $HOME/Documents/Other/
+mkdir -p $HOME/Documents/PDFs/
+mkdir -p $HOME/Downloads/
+mkdir -p $HOME/Downloads/Torrents/
+mkdir -p $HOME/Music/
+mkdir -p $HOME/Music/Study-Work/
+mkdir -p $HOME/Music/Workout/
+mkdir -p $HOME/Pictures/
+mkdir -p $HOME/Pictures/Screenshots/
+mkdir -p $HOME/Repositories/
+mkdir -p $HOME/Repositories/Other-Repositories/
+mkdir -p $HOME/Repositories/User-Repositories/
+mkdir -p $HOME/Videos/
+
 rm -rf $HOME/README.md
 rm -rf $HOME/install.sh
